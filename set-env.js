0f3400fc2c529d125da0429b212db6b1891dae83
JoaoMaganin <joaovitormontemor138@gmail.com>
2026-06-27 17:57:25 -0300

    test: variables in prod

--- set-env.js
+++ set-env.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY ? 'definida' : 'indefinida');
+console.log('TMDB_TOKEN:', process.env.TMDB_TOKEN ? 'definido' : 'indefinido');
+
 const content = `
 export const environment = {
   production: true,
